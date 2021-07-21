461e2e08570665a0eb3e0b1ec14839d3a23c89f2
Yash <ysomani07@gmail.com>
2021-07-21 12:55:59 +0530

    fragment created for bottom bar and different application like setting, home, profile and search

--- app/src/main/java/com/example/groupwork/MainActivity.java
+++ app/src/main/java/com/example/groupwork/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.groupwork;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.WindowManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //      Transparent ActionBar{
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 //      }
         getSupportActionBar().hide();
 
