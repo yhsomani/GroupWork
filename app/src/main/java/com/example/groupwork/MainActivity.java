a49575006f36d84354e6223673e915ebe5ab5bd8
Yash <ysomani07@gmail.com>
2021-07-21 18:47:04 +0530

    bottom_nav_menu and fragment created

--- app/src/main/java/com/example/groupwork/MainActivity.java
+++ app/src/main/java/com/example/groupwork/MainActivity.java
@@ -1,14 +1,10 @@
 package com.example.groupwork;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.WindowManager;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,10 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 //      Transparent ActionBar{
 //        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 //      }
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
 
     }
 }
