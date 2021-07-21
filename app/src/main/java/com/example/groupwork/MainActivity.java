59d92e29e62a78a5d54b794befc9c9cda1b8fa58
Yash <ysomani07@gmail.com>
2021-07-21 19:15:32 +0530

    Fragement added on bottom nav bar

--- app/src/main/java/com/example/groupwork/MainActivity.java
+++ app/src/main/java/com/example/groupwork/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.groupwork;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +23,35 @@ public class MainActivity extends AppCompatActivity {
 //        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 //      }
         Objects.requireNonNull(getSupportActionBar()).hide();
+        bottomNavigation = findViewById(R.id.bottomNavigationView);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(home_fregment.newInstance("", ""));
 
     }
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.Home:
+                    openFragment(home_fregment.newInstance("", ""));
+                    return true;
+                case R.id.Search:
+                    openFragment(search_fragment.newInstance("", ""));
+                    return true;
+                case R.id.Person:
+                    openFragment(profile_fragment.newInstance("", ""));
+                    return true;
+                case R.id.Setting:
+                    openFragment(search_fragment.newInstance("", ""));
+                    return true;
+            }
+            return false;
+        }
+    };
 }
